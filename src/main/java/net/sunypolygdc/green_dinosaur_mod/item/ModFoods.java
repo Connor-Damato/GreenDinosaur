132f80ef6d469e6c73ffca6151615d026727955c
Litorom <litorom1@gmail.com>
2024-02-03 19:03:40 -0500

    Fixed: Eating speed

--- src/main/java/net/sunypolygdc/green_dinosaur_mod/item/ModFoods.java
+++ src/main/java/net/sunypolygdc/green_dinosaur_mod/item/ModFoods.java
@@ -5,9 +5,9 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 
 public class ModFoods {
-    public static final FoodProperties Dino_fruit = new FoodProperties.Builder().nutrition(4).fast()
+    public static final FoodProperties Dino_fruit = new FoodProperties.Builder().nutrition(4)
             .saturationMod(0.2f).build();
-    public static final FoodProperties Blue_fruit = new FoodProperties.Builder().nutrition(4).fast()
+    public static final FoodProperties Blue_fruit = new FoodProperties.Builder().nutrition(4)
             .saturationMod(0.2f).build();
 
 }
